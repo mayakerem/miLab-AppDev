ddbb1ea42efbcc4fba7bfd6398b8de5fd02e95bf
Maya Kerem <mayakerem21@gmail.com>
2019-01-18 16:53:46 +0200

    able to send stock name to server, and in the server, able to get price. TODO: sucessfully send the price back to the client

--- EX6/Server/index.js
+++ EX6/Server/index.js
@@ -5,7 +5,7 @@ The server will monitor the last price of the stock and send the client price up
 Start out with a live connection using Socket.IO. 
 Once that works, you can implement push with Firebase in case the Android app isn’t open
 */
-console.log("starting server ");
+console.log("V8 - starting server ");
 const express = require('express');
 const app = express();
 // Set up the server
@@ -18,8 +18,6 @@ const alpha = require('alphavantage')({ key: 'EFCR3UVVCE2EYRSI' });
 console.log("set up Alpha");
 const PORT = process.env.PORT || 8080;
 console.log(`choose port: ${PORT}`);
-//set up app to the right PORT
-console.log("after app = express");
 
 app.set('port',PORT);
 app.use(express.static(__dirname + '/public'));
@@ -30,34 +28,40 @@ io.on('connection', function(socket){
   console.log("Client connected");
 
   //when client emits a stock name, this listens and executes
-  socket.on('sendStockName', function(data) {
+  socket.on('sendStockName', function(stockName) {
     //listens every 5 seconds
     console.log("begining interval");
-    if (data == undefined) {
-      console.log("Data is undefined");
-      
-    }
-
-    setInterval(() => {
-      console.log("data: " + data);
-      socket.emit('sendStockName',{
-          Data: retrieveData(data)
-        }), 5000;
+    // if (stockName == undefined) {
+    //   console.log("stockName is undefined");
+    // }
+   // setInterval(() => {
+      alpha.data.batch([`${stockName}`]).then(data => {
+        console.log("price: " + `${data['Stock Quotes'][0]['2. price']}`);
+        socket.emit('sendStockData',{
+          symbol: `${stockName}`,
+          price: `${data['Stock Quotes'][0]['2. price']}`, 
+        });
+      }).catch(err => {
+        console.error("Error! -> " + err);
+     // }), 15000;
       }); 
   });
 });
 
-//function that takes the data
-function retrieveData(name){
-  console.log("inside retrieveData");
-  alpha.data.batch([`${name}`]).then(data => {
-    let value = `${data['Stock Quotes'][0]['2. price']}`;
-    return(`${name} -> ${data['Stock Quotes'][0]['2. price']}`);
-  //for every then we need to have a catch
-  }).catch(err => {
-    console.error("Error! -> " + err);
-});
-};
+// //function that takes the data
+// function retrieveData(name){
+//   console.log("inside retrieveData");
+//   alpha.data.batch([`${name}`]).then(data => {
+//     socket.broadcast.emit('sendStockData', {
+//       symbol: `${name}`,
+//       price: `${data['Stock Quotes'][0]['2. price']}`, 
+//     })
+//     //return(`${name} -> ${data['Stock Quotes'][0]['2. price']}`);
+//   //for every then we need to have a catch
+//   }).catch(err => {
+//     console.error("Error! -> " + err);
+// });
+// };
 
 //listining to the URL
 server.listen(PORT, () => {
--- EX6/StockData/app/src/main/java/com/example/stockdata/MainActivity.java
+++ EX6/StockData/app/src/main/java/com/example/stockdata/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -19,10 +20,12 @@ import io.socket.emitter.Emitter;
 public class MainActivity extends AppCompatActivity {
 
     private String port = "8080";
+    // Emulator IP
     private String host = "http://10.0.2.2:";
     private Socket mSocket;
     String name;
 
+    //Socket Set Up
     {
         try {
             mSocket = IO.socket(host + port);
@@ -31,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
             Log.d("Debug", "not connected");
         }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +46,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Retrieve stock name
-                EditText nameField = (EditText) findViewById(R.id.editText);
-                name = nameField.getText().toString().trim();
-                //Send stock name and open server communication
+                EditText editTextField = (EditText) findViewById(R.id.editText);
+                name = editTextField.getText().toString().trim();
                 Log.d("Debug", "going to connect to server now");
+                //Send stock name and open server communication
                 sendToSocket();
             }
         });
 
         Button stop_btn = (Button) findViewById(R.id.StopReceiving);
+        //Click on stop button to stop receiving data
         stop_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
+                    //When clicked, close the socket
                     mSocket.close();
                 } catch (Exception e) {
                     Log.d("Debug", "error when closing socket");
@@ -65,41 +69,48 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Socket communication function
     private void sendToSocket() {
         try {
-            //open socket with
-//            mSocket = IO.socket(host + port);
+            //Connect to the server that was set up before
             mSocket.connect();
-//            Log.d("Debug", "has connected");
             if (mSocket != null){
                 Log.d("Debug", "Socket is connected");
-                //send
+                //send the socket name to the event "sendStockName"
                 mSocket.emit("sendStockName", name );
-
-
-                //listining
-                mSocket.on("sendStockName", new Emitter.Listener() {
+                //Now start listening to the event from the server with an Event Listener
+                mSocket.on("sendStockData", new Emitter.Listener() {
                     @Override
                     public void call(final Object... args) {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 //creating a JSON file
-                                JSONObject data = (JSONObject) args[0];
-                                Log.d("Debug", "recieved JSON");
+                                JSONObject json = (JSONObject) args[0];
+                                Log.d("Debug", "received JSON");
+                                String symbol = "";
+                                String price = "";
+                                try {
+                                    symbol = json.getString("data");
+                                    price = json.getString("price");
+                                    Log.d("Debug", "The provided name: " + symbol);
+                                    Log.d("Debug", "The Price: " + price);
+                                } catch (JSONException e) {
+                                    Log.d("Debug","Didnt get the JSON");
+                                }
 
                             }
                         });
                     }
                 });
             } else {
-                Log.d("Debug", "mSocket didnt connect");
+                Log.d("Debug", "mSocket didn't connect");
             }
         } catch (Exception e) {
             Log.d("Debug", "Socket exception");
-        }
-        finally {
-            Log.d("Debug", "Closing the socket! ");
+//        }
+//        finally {
+//            Log.d("Debug", "Closing the socket! ");
         }
     }
 
